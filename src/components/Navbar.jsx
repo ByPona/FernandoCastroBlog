755c96259a8ab144d789e543768f3099be32a6e2
Fernando Castro <play_fer01@hotmail.com>
2026-03-18 14:27:47 -0600

    update banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a href="#home" className="navbar-logo">Fernando Castro Software Engineer in Test</a>
+        <a href="#home" className="navbar-logo">Fernando Castro - Software Engineer in Test</a>
         <ul className="navbar-menu">
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
